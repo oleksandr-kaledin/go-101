34424fb5674bddefe6dbf037ceb468a5c5d1da7f
your_username <your_email@gmail.com>
2022-07-13 13:20:37 +0300

    update

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -3,10 +3,9 @@ package handler
 import "github.com/gin-gonic/gin"
 
 type Handler struct {
-
 }
 
-func (h *Handler)initRoutes() *gin.Engine {
+func (h *Handler)InitRoutes() *gin.Engine {
 	router := gin.New()
 
 	auth := router.Group("/auth")
